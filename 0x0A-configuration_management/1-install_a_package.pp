5fdd364d7a4847b55cac76b09ce939bd8d451a9d
mg5714 <mistergddora2000@gmail.com>
2024-03-23 04:08:46 +0200

    go

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# install flask from pip3.
-package {'flask':
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Install Flask package
+package { 'python3-flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
