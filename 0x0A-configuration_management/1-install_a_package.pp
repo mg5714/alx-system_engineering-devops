fc91fcae220923d6a8b48519982b94e028d16c52
mg5714 <mistergddora2000@gmail.com>
2024-03-24 17:19:45 +0200

    go

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install flask from pip3.
 
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '1.0.1',  # or another compatible version
+  provider => 'pip3',
 }
